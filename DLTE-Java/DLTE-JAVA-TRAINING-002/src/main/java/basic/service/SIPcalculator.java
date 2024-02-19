98661cc7680743bda971342c3e5219866fe69389
akashh01 <akashdins0101@gmail.com>
2024-02-19 20:25:45 +0530

    SIP calculator

--- DLTE-Java/DLTE-JAVA-TRAINING-002/src/main/java/basic/service/SIPcalculator.java
+++ DLTE-Java/DLTE-JAVA-TRAINING-002/src/main/java/basic/service/SIPcalculator.java
@@ -5,23 +5,29 @@ import java.util.function.LongFunction;
 
 public class SIPcalculator {
     public static void main(String[] args) {
-//        SIP calculator
+//        SIP calculator -systematic investment plan.
 //        Inputs: monthlyInvestment, expectedReturnsInPer, Period in years
 //        Output: principle  Amount, Estimated returns, Total
-        Long monthlyInvestment,totalNumofInvestments,expectedReturnsInPer,monthlyIntrestRate;
-        double totalReturn;
-        int numOfMonths;
+        Long monthlyInvestment;
+        double totalReturn,expectedReturnsPerYear, monthlyIntrestRate,EstimatedReturn,totalMoneyInvested;
+        int numOfMonths,NumofYears;
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter your Monthly Investment");
         monthlyInvestment=sc.nextLong();
         System.out.println("Enter expected return in %");
-        expectedReturnsInPer=sc.nextLong();
+        expectedReturnsPerYear=sc.nextDouble();
         System.out.println("Enter time period in years");
-        totalNumofInvestments=sc.nextLong();
-        monthlyIntrestRate=totalNumofInvestments/12/100;
-        numOfMonths= (int) (12*totalNumofInvestments);
-        totalReturn=monthlyInvestment*((Math.pow((expectedReturnsInPer+1),totalNumofInvestments)/expectedReturnsInPer)*(expectedReturnsInPer+1));
-        System.out.println(totalReturn);
+        NumofYears=sc.nextInt();
+        //converting return % per year to per months
+        monthlyIntrestRate=expectedReturnsPerYear/12/100;
+        numOfMonths=12*NumofYears;
+        //formula for calculating sip
+        totalReturn=monthlyInvestment*((Math.pow((1+monthlyIntrestRate),(numOfMonths))-1)*(1+monthlyIntrestRate))/monthlyIntrestRate;
+        //total money invested,estimatedreturn ,total return
+        totalMoneyInvested=numOfMonths*monthlyInvestment;
+        EstimatedReturn=totalReturn-totalMoneyInvested;
+        System.out.println("Total amount invested ="+totalMoneyInvested+"\nEstimated Return ="+EstimatedReturn+"\nTotal Return ="+totalReturn);
+
 
 
     }
