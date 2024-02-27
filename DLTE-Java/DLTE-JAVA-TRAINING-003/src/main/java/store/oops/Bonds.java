062e325c29e7bd30e520324a0c7610fc6d168f54
akashh01 <akashdins0101@gmail.com>
2024-02-27 17:55:21 +0530

    bonds updated again

--- DLTE-Java/DLTE-JAVA-TRAINING-003/src/main/java/store/oops/Bonds.java
+++ DLTE-Java/DLTE-JAVA-TRAINING-003/src/main/java/store/oops/Bonds.java
@@ -19,32 +19,47 @@ public class Bonds {
 
     public static void main(String[] args) {
         //defining 5 different bonds
-        Bonds bonds[]={
-                new Bonds("Government bond",5,7.2,"No tax",5),
-                new Bonds("Corporate bond",5,12.5,"No tax",5),
-                new Bonds("Municiple bond",5,5.5,"No tax",5),
-                new Bonds("Agency bond",5,6.0,"No tax",8),
-                new Bonds("Agency bond",5,9.0,"tax of 10%",7)
-      };
-        int position= MaximumReturn(bonds);
-     //   System.out.println(bonds[position]);
+        Bonds bonds[] = {
+                new Bonds("Government bond", 5, 1.2, "No tax", 5),
+                new Bonds("Corporate bond", 5, 12.5, "No tax", 5),
+                new Bonds("Municiple bond", 5, 5.5, "No tax", 5),
+                new Bonds("Agency bond", 5, 6.0, "No tax", 8),
+                new Bonds("Agency bond", 5, 9.0, "tax of 10%", 7)
+        };
+       // int position = MaximumReturn(bonds);
+        MaximumReturn(bonds);
+        //   System.out.println(bonds[position]);
     }
-     //Maximum Interests
-    public static int MaximumReturn(Bonds[] bonds){
-        double maximumIntrest=0;
-        String bondName="";
-        int postion=0,count=0;
-        for(Bonds each:bonds){
-            count++;
-            if(each.getInterestRate()>maximumIntrest){
-                maximumIntrest=each.getInterestRate();
-                bondName=each.getBondName();
-                postion=count;
+
+    //Maximum Interests
+    public static void MaximumReturn(Bonds[] bonds) {
+        double maximumIntrest = 0;
+        Bonds newBond = null;
+       // int maxPositon = 0;
+        for (int index =0;index<bonds.length-1;index++) {
+            maximumIntrest = bonds[index].getInterestRate();
+            int maxPositon=index;
+            for (int next=index+1; next<bonds.length; next++) {
+                if (maximumIntrest < bonds[next].getInterestRate()) {
+                    maximumIntrest = bonds[next].getInterestRate();
+                    maxPositon = next;
+                }
             }
+                if (maxPositon!=index) {
+                    newBond=bonds[maxPositon];
+                    bonds[maxPositon]=bonds[index];
+                    bonds[index]=newBond;
+                }
+
+
         }
-        System.out.println("The maximum intrest rate is "+maximumIntrest+ " for the "+bondName);
-        return postion;
+        for (Bonds each : bonds) {
+            System.out.println(each.getInterestRate());
+        }
     }
-
-
 }
+
+
+
+
+
