2df9ad5e4372afabfafba0a99541a4927a6075b1
akashh01 <akashdins0101@gmail.com>
2024-03-18 17:49:39 +0530

    soap changes done

--- DLTE-javaEE/Tasks/Soap_readAll_client/src/main/java/org/example/App.java
+++ DLTE-javaEE/Tasks/Soap_readAll_client/src/main/java/org/example/App.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import web.AccountDetails;
+import web.Customer;
+import web.ReadAccount;
+import web.ReadAccountService;
+
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+       // ReadAccountService readAccountService=new ReadAccountService();
+    //    AccountDetails accountDetails= (AccountDetails) readAccountService.getReadAccountPort();
+        ReadAccountService service=new ReadAccountService();
+        ReadAccount accountSoap=service.getReadAccountPort();
+
+        //AccountDetails accountSoap = (AccountDetails) service.getReadAccountPort();
+        List<Customer> cards = accountSoap.readAll("Eeksha").getDetails();
+        for(Customer each:cards){
+            System.out.println(each.getUsername()+" "+each.getPassword()+" "+each.getInitialBalace());
+        }
+
     }
 }
